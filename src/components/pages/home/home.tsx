6f1ab6ffb5f5be56650da28a763fc93f5de8fa8d
Ishan Khurram <mo.ishan.khurram@gmail.com>
2024-04-05 13:05:37 -0400

    Added typewriter effect as well as satoshi fonts.

--- src/components/pages/home/home.tsx
+++ src/components/pages/home/home.tsx
@@ -1,20 +1,59 @@
-import React from 'react';
-import { motion } from "framer-motion"
-import logo from 'assets/images/iklogo.svg'
-import 'components/pages/home/home.css'
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import logo from "assets/images/iklogo.svg";
+import "components/pages/home/home.css";
+import "fonts/Satoshi-Black.ttf"
 
 const Home = () => {
+  const fullText = "Hi, I'm Ishan Khurram";
+  const [text, setText] = useState("");
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setText((currentText) => {
+        if (currentText.length < fullText.length) {
+          return fullText.substring(0, currentText.length + 1);
+        }
+        clearInterval(interval);
+        return currentText;
+      });
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, [fullText]);
 
   return (
     <div className="home">
-      {/* Logo svg */}
-      <motion.img id="logo" src={logo} alt="Logo"
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-        drag // Optional: enable dragging
-        dragConstraints={{ left: 0, top: 0, right: 0, bottom: 0 }} // Optional: constrains dragging
-      />
+      <div className="logo">
+        <motion.img
+          id="logo"
+          src={logo}
+          alt="Logo"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        />
+      </div>
+      <div className="title1">
+        <div className="typing-animation">
+          {text}
+          <span className="blinking-cursor">|</span>
+        </div>
+      </div>
+      <div className="title2">
+        <div className="typing-animation">
+          {text}
+          <span className="blinking-cursor">|</span>
+        </div>
+      </div>
+      <div className="title3">
+        <div className="typing-animation">
+          {text}
+          <span className="blinking-cursor">|</span>
+        </div>
+      </div>
+      <div className="intro">
+        <p>I am an aspiring frontend developer with a passion for turning visions into a reality.</p>
+      </div>
     </div>
   );
 };
